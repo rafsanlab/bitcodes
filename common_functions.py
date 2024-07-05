1cbaa95b7a192c8497b8d8fd7bb8b6bba1dec642
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2024-07-05 15:16:12 +0100

    Update common_functions.py
    add collect_same_file_from_subdir

--- common_functions.py
+++ common_functions.py
@@ -12,7 +12,7 @@ import pathlib
 import re
 import urllib.request
 import zipfile
-
+from natsort import natsorted
 
 def get_data(path:str, file_type:str, verbose:bool):
   """
@@ -284,6 +284,32 @@ def match_fname(list1:list, list2:list, method:str='direct',
 
     return file_dict
 
+
+def collect_same_file_from_subdir(projectdir, subsubdir=None, fname='', filter_subdir_names:list=None):
+    """ collect file paths from subdir of projectdir if all files are the same name.
+        returns dict containing the subdir (key) and filepath (value).
+    """
+    filepaths = {}
+    # get all the data from subdir
+    for subdir in os.listdir(projectdir):
+        if os.path.isdir(os.path.join(projectdir, subdir)): # avoid files, just folders
+            if subsubdir is not None: # a subdir of subdir
+                filepath = os.path.join(subdir, subsubdir, fname)
+            else:
+                filepath = os.path.join(subdir, fname)
+            filepath = os.path.join(projectdir, filepath)
+            if os.path.exists(filepath): # check is exist
+                filepaths[subdir] = filepath
+    # filter subdir name based on filter_subdir_name list
+    if filter_subdir_names is not None:
+        filtered_filepaths = {}
+        for k, v in filepaths.items():
+            if all(name in k for name in filter_subdir_names):
+                filtered_filepaths[k] = v
+        filepaths = filtered_filepaths
+    # natural sort the keys
+    filepaths = {k: filepaths[k] for k in natsorted(filepaths)}
+    return filepaths
 
 # def match_fname(list1:list, list2:list, method='direct', split_pattern=None, split_n=None):
 #     """
